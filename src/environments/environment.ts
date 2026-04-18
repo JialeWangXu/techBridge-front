b378a3a37d18fc149675d30bd696f0de271e6d9b
JialeWangXu <jiale.wang@alumnos.upm.es>
2026-04-18 18:27:28 +0200

    fixed refreshToken and replaced it by silence renew technic #35

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
     VERSION: pkg.version,
     FRONT_END: 'http://localhost:4200',
     REST_USER: 'http://localhost:8080/api/techbridge-user',
-    SECURE_ROUTES: ['http://localhost:8080'],
+    SECURE_ROUTES: ['http://localhost:8080/api'],
 };
 
