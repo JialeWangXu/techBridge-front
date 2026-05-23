47d2065727f8e0d20cd6876291c548c4de9b9d81
JialeWangXu <jiale.wang@alumnos.upm.es>
2026-05-23 12:35:41 +0200

    added aws lightsail into cd pipeline

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,12 +4,12 @@ export const environment = {
     production: true,
     NAME: pkg.name,
     VERSION: pkg.version,
-    FRONT_END: 'http://techbridge-front.s3-website-eu-west-1.amazonaws.com',
-    BACK_END: 'http://34.240.99.239:8080',
-    REST_USER: 'http://34.240.99.239:8080/api/techbridge-user',
+    FRONT_END: 'https://jointechbridge.com',
+    BACK_END: 'https://jointechbridge.com/api',
+    REST_USER: 'https://jointechbridge.com/api/techbridge-user',
     SECURE_ROUTES: [
-    'http://34.240.99.239:8080/api/techbridge-user/users',
-    'http://34.240.99.239:8080/api/techbridge-helprequest',
-    'http://34.240.99.239:8080/api/techbridge-aitutorial',
+    'https://jointechbridge.com/api/techbridge-user/users',
+    'https://jointechbridge.com/api/techbridge-helprequest',
+    'https://jointechbridge.com/api/techbridge-aitutorial',
     ],
 };
